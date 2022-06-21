d75052fece539134e2bb472bba8fc83e8af6d26d
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-06-21 22:47:33 +0200

    refactorybuttonQuitter

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', function (e){
         username.style.border="1px solid red";
     } else{
         document.getElementById("erreurNom").innerText=""
-        username.style.border="1px solid green"
+        //username.style.border="1px solid green"
     }
     if (email.value.length == 0){
         document.getElementById("erreurMail").innerText = "N’oubliez pas de renseigner votre email avant de commencer le Quiz";
@@ -23,18 +23,29 @@ form.addEventListener('submit', function (e){
 
     }else{
         document.getElementById("erreurMail").innerText=""
-        email.style.border="1px solid green"
+        //email.style.border="1px solid green"
     }
    
 });
 
 const buttonBegin = document.querySelector("#buttonBegin");
 const quizOne = document.querySelector(".quizOne");
+const sectionLogin =document.querySelector('.sectionLogin');
+const quitter = document.querySelector('.quitter')
 
    buttonBegin.addEventListener('click', () => {
     if(quizOne.style.display === 'none') {
         quizOne.style.display = 'block';
+        sectionLogin.style.display ='none';
        } else {
         quizOne.style.display = 'none';
        }
+   });
+   quitter.addEventListener('click', () => {
+    if(buttonBegin.style.display === 'none') {
+    buttonBegin.style.display ='block';
+    quizOne.style.display ='none';
+    }else{
+        buttonBegin.style.display ='none';
+    }
    });
