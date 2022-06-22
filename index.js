b4d9ebd43503062020ccd7f1597b19a8266adf0a
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-06-22 14:54:15 +0200

    refactoryScript

--- index.js
+++ index.js
@@ -4,29 +4,33 @@
 const form = document.getElementById('form');
 
 let error = []
-form.addEventListener('submit', function (e){
-    e.preventDefault();
+const username = document.getElementById('username');
+const email = document.getElementById('email');
 
-    const username = document.getElementById('username');
-    const email = document.getElementById('email');
-
+function checkIdentityName(){
     if (username.value.length == 0){
         document.getElementById("erreurNom").innerText="N’oubliez pas de renseigner votre nom avant de commencer le Quiz.";
         username.style.border="1px solid red";
+        return false;
     } else{
         document.getElementById("erreurNom").innerText=""
         //username.style.border="1px solid green"
-    }
+        return true;
+    }   
+}
+
+function checkIdentityMail() {
     if (email.value.length == 0){
         document.getElementById("erreurMail").innerText = "N’oubliez pas de renseigner votre email avant de commencer le Quiz";
         email.style.border="1px solid red";
-
+        return false
     }else{
         document.getElementById("erreurMail").innerText=""
         //email.style.border="1px solid green"
+        return true;
     }
-   
-});
+}
+
 
 const buttonBegin = document.querySelector("#buttonBegin");
 const quizOne = document.querySelector(".quizOne");
@@ -39,16 +43,18 @@ const accueil = document.getElementById("accueil");
 
    
 
-   buttonBegin.addEventListener('click', () => {
-    if(quizOne.style.display === 'none') {
-        quizOne.style.display = 'block';
+   buttonBegin.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (checkIdentityMail()==true && checkIdentityName()==true) {
         sectionLogin.style.display ='none';
-       } else {
-        quizOne.style.display = 'none';
-       }
+        quizOne.style.display = 'block';
+    } else {
+        console.log("non");
+    }
+    
    });
 
-   quitter.addEventListener('click', () => {
+   quitter.addEventListener('click', () => {    
     if(end1.style.display === 'none') {
         end1.style.display ='block';
     quizOne.style.display ='none';
